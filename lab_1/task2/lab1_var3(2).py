6d401a5fac9c087bb33e663856669b8e5dbb09fd
tsda04 <tsda04@mail.ru>
2024-09-29 02:32:19 +0400

    task2 upd. with exception handling

--- lab_1/task2/lab1_var3(2).py
+++ lab_1/task2/lab1_var3(2).py
@@ -5,29 +5,53 @@ import my_key_2 as my_key_2
 def frequency_analysis(text: str) -> list:
     """
     Проводит частотный анализ текста.
+
     Args:
         text (str): Текст для анализа.
+
     Returns:
         list: Список пар (буква, частота).
     """
     frequency = collections.Counter(text)
     total = sum(frequency.values())
     sorted_frequency = sorted(frequency.items(), key=lambda x: x[1])
-    result = [(char, count / total) for char, count in sorted_frequency if char != "\n"]
+    result = [(char, count / total)
+              for char, count in sorted_frequency if char != "n"]
     return result
 
 
 def create_key(text: str) -> dict:
+    """
+    Создает ключ сопоставляя результаты частотного анализа текста
+    и частоту букв в русском алфавите
+
+    Args:
+        text (str): Зашифрованный текст.
+
+    Returns:
+        dict: Словарь с ключами для декодирования (буква1: буква2)
+    """
     freq_cipher = frequency_analysis(text)
     freq_ru = {}
-    with open("frequencies.txt", "r", encoding='utf-8') as f2:
-        for line in f2:
-            char, freq = line.split('=')
-            freq_ru[char] = float(freq)
+    try:
+        with open("frequencies.txt", "r", encoding='utf-8') as f2:
+            for line in f2:
+                char, freq = line.split('=')
+                freq_ru[char] = float(freq)
+    except FileNotFoundError:
+        print("Файл frequencies.txt не найден. Ключ не может быть создан.")
+        return {}
+    except Exception as e:
+        print(f"Ошибка при чтении файла frequencies.txt: {e}")
+        return {}
 
     # Сортировка по убыванию частоты
-    sorted_freq_cipher = sorted(freq_cipher, key=lambda x: x[1], reverse=True)
-    new_key = {fc[0]: fr[0] for fc, fr in zip(sorted_freq_cipher, freq_ru)}
+    sorted_freq_cipher = sorted(
+        freq_cipher, key=lambda x: x[1], reverse=True)
+    new_key = {
+        fc[0]: fr[0]
+        for fc, fr in zip(sorted_freq_cipher, freq_ru)
+    }
     return new_key
 
 
@@ -37,7 +61,7 @@ def decrypt(text: str, key: dict) -> str:
 
     Args:
         text (str): Зашифрованный текст.
-        key (str): Ключ для расшифровки.
+        key (dict): Ключ для расшифровки.
 
     Returns:
         str: Расшифрованный текст.
@@ -54,23 +78,36 @@ def decrypt(text: str, key: dict) -> str:
 
 if __name__ == "__main__":
 
-    with open("cod3.txt", "r", encoding='utf-8') as f1:
-        cipher_text = f1.read()
+    try:
+        with open("cod3.txt", "r", encoding='utf-8') as f1:
+            cipher_text = f1.read()
+    except FileNotFoundError:
+        print("Файл cod3.txt не найден. Дешифровка невозможна.")
+        exit()
+    except Exception as e:
+        print(f"Ошибка при чтении файла cod3.txt: {e}")
+        exit()
 
     if len(my_key_2.key) == 0:
         # если ключ еще не был создан
         key = create_key(cipher_text)
-        print(f'Ключ шифрования: (для дальнейшей работы с ним вставьте его в файл my_key_2.py)')
-        print(key)
-
+        if key:  # Проверяем, что ключ был создан успешно
+            print(
+                f'Ключ шифрования: (для дальнейшей работы с ним вставьте его в файл my_key_2.py)')
+            print(key)
+        else:
+            exit()  # Выход из программы, если ключ не создан
     else:
         # если ключ был создан
         key = my_key_2.key
         print(f'Ключ шифрования загружен из my_key_2.py')
 
     deciphered_text = decrypt(cipher_text, key)
-    with open('cod3_decrypted.txt', 'w', encoding='utf-8') as f:
-        f.write(deciphered_text)
-    print(f'Расшифрованный текст записан в cod3_decrypted.txt')
-    print()
-    print(deciphered_text)
+    try:
+        with open('cod3_decrypted.txt', 'w', encoding='utf-8') as f:
+            f.write(deciphered_text)
+        print(f'Расшифрованный текст записан в cod3_decrypted.txt')
+        print()
+        print(deciphered_text)
+    except Exception as e:
+        print(f"Ошибка при записи в файл: {e}")
