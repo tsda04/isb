14e85da1ef846e75fdcc507db9e941b7688893ee
tsda04 <tsda04@mail.ru>
2024-09-29 00:50:20 +0400

    task2 upd.

--- lab_1/task2/lab1_var3(2).py
+++ lab_1/task2/lab1_var3(2).py
@@ -1,8 +1,8 @@
-import os
 import collections
 import key_2
 
-def frequency_analysis(text : str) -> list:
+
+def frequency_analysis(text: str) -> list:
     """
     Проводит частотный анализ текста.
     Args:
@@ -13,22 +13,25 @@ def frequency_analysis(text : str) -> list:
     frequency = collections.Counter(text)
     total = sum(frequency.values())
     sorted_frequency = sorted(frequency.items(), key=lambda x: x[1])
-    result = [(char, count / total) for char, count in sorted_frequency]
+    result = [(char, count / total) for char, count in sorted_frequency if char != "\n"]
     return result
 
-def create_key(cipher_text : str) -> dict:
-    freq_cipher = frequency_analysis(cipher_text)
+
+def create_key(text: str) -> dict:
+    freq_cipher = frequency_analysis(text)
     freq_ru = {}
-    with open("frequencies.txt", "r", encoding='utf-8') as f:
-        for line in f:
+    with open("frequencies.txt", "r", encoding='utf-8') as f2:
+        for line in f2:
             char, freq = line.split('=')
             freq_ru[char] = float(freq)
 
     # Сортировка по убыванию частоты
     sorted_freq_cipher = sorted(freq_cipher, key=lambda x: x[1], reverse=True)
-    key = {fc[0]: fr[0] for fc, fr in zip(sorted_freq_cipher, freq_ru)}
-    return key
-def decrypt(text : str, key : str) -> str:
+    new_key = {fc[0]: fr[0] for fc, fr in zip(sorted_freq_cipher, freq_ru)}
+    return new_key
+
+
+def decrypt(text: str, key: dict) -> str:
     """
     Расшифровывает текст с помощью ключа.
 
@@ -40,27 +43,34 @@ def decrypt(text : str, key : str) -> str:
         str: Расшифрованный текст.
     """
     translated = ''
-    chars_a = key
-    chars_b = ' ОИЕАНТСРВМЛДЯКПЗЫЬУЧЖГХФЙЮБЦШЩЭЪ'
+    chars_a = key.keys()
     for symbol in text:
         if symbol in chars_a:
-            sym_index = chars_a.find(symbol)
-            translated += chars_b[sym_index]
+            translated += key.get(symbol)
         else:
             translated += symbol
     return translated
 
+
 if __name__ == "__main__":
 
-    with open("cod3.txt", "r", encoding='utf-8') as f:
-        cipher_text = f.read()
+    with open("cod3.txt", "r", encoding='utf-8') as f1:
+        cipher_text = f1.read()
 
-    key = create_key(cipher_text)
-    print(f'Ключ шифрования: (для дальнейшей работы с ним вставьте его в файл key_2.py)')
-    print(key)
+    if len(key_2.key) == 0:
+        # если ключ еще не был создан
+        key = create_key(cipher_text)
+        print(f'Ключ шифрования: (для дальнейшей работы с ним вставьте его в файл key_2.py)')
+        print(key)
 
-    deciphered_text = decrypt(cipher_text, ''.join([k for k, _ in key.items()]))
+    else:
+        # если ключ был создан
+        key = key_2.key
+        print(f'Ключ шифрования загружен из key_2.py')
+
+    #deciphered_text = decrypt(cipher_text, ''.join([k for k, _ in key.items()]))
+    deciphered_text = decrypt(cipher_text, key)
     with open('cod3_decrypted.txt', 'w', encoding='utf-8') as f:
         f.write(deciphered_text)
-
     print(f'Расшифрованный текст записан в cod3_decrypted.txt')
+    print(deciphered_text)
